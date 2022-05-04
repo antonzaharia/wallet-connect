a7f265fa9c046c4a16f4be50db1ce4b50043adc2
Anton Zaharia <zaharia_anton@yahoo.com>
2022-05-04 12:55:20 +0100

    basic style and layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const handleOnClick = () => {
+    console.log('connect!')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Wallet connect</header>
+      <div className="content">
+        <button className="connect-button" onClick={handleOnClick}>
+          Connect
+        </button>
+        <div className="results"></div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
